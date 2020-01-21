2e2e2cfd7b0758aaac79151d93e8f4c7746dd878
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2020-01-21 07:55:05 +0100

    Profile with styling

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+
+  return (
+    <div className="profile">
+      <div className="profile__picture">
+        <div className="profile__image"></div>
+      </div>
+      <div className="profile__text">
+        <div className="profile__paragraph">"
+        Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yueha
+        Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yuehas
+        "</div>
+      </div>
+      <div className="profile__links">
+        <div className="profile__links--type"></div>
+        <div className="profile__links--type"></div>
+        <div className="profile__links--type"></div>
+        <div className="profile__links--type"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
