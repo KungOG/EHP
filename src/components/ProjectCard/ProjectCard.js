eae80aa3c6c083dbebd54aa4d87d9143b0e36aef
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2020-01-21 07:55:35 +0100

    Project with styling

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const ProjectCard = () => {
+
+  return (
+    <div className="project">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default ProjectCard;
