24ec2eff6d72335f7f5c66da5801a46096f9fa8f
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2020-01-22 19:35:55 +0100

    Added name tag and work tag

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,10 +8,12 @@ const Profile = () => {
         <div className="profile__image"></div>
       </div>
       <div className="profile__text">
-        <div className="profile__paragraph">"
-        Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yueha
-        Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yuehas
-        "</div>
+        <h1 className="profile__name">Namn Namnsson</h1>
+        <h2 className="profile__work">Yrke Yrke Yrke</h2>
+        <div className="profile__paragraph">
+        "Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yueha
+        Lorem ipsum yadhe yadhelr hadjaeu eyadehehe yuehas"
+        </div>
       </div>
       <div className="profile__links">
         <div className="profile__links--type"></div>
