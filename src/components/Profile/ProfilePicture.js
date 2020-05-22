d5ea0e50d115089c184b94cc8fbeb93f6a7e5f3e
Oscar Gustafsson <oscar-gustafsson@live.com>
2020-05-22 16:29:40 +0200

    Removed Profile and added 3 components for Profile instead

---
+++ src/components/Profile/ProfilePicture.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProfilePicture = (props) => {
+  const { ProfilePicture } = props;
+
+  return (
+    <div className="profile__picture">
+      <div
+        className="profile__image"
+        style={{ backgroundImage: `${ProfilePicture}` }}
+      />
+    </div>
+  );
+};
+
+ProfilePicture.propTypes = {
+  ProfilePicture: PropTypes.string,
+};
+
+export default ProfilePicture;
